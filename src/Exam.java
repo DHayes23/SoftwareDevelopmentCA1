4d743ff0254aafc27007944cc21d4797b85eaf29
DHayes23 <d.hayesdeveloper@gmail.com>
2023-11-05 13:10:20 +0000

    Add abstract Exam Class and ExamException class.

---
+++ src/Exam.java
@@ -0,0 +1,40 @@
+public abstract class Exam {
+    protected int examId;
+    protected String subject;
+    protected int duration;
+
+    public Exam(int examId, String subject, int duration) throws ExamException {
+        setDuration(duration); // Using the setter to enforce the rule within the constructor
+        this.examId = examId;
+        this.subject = subject;
+    }
+
+    // Getters
+    public int getExamId() {
+        return examId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    // Setters
+    public void setExamId(int examId) {
+        this.examId = examId;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setDuration(int duration) throws ExamException {
+        if (duration < 30 || duration > 180) {
+            throw new ExamException("Duration must be between 30 and 180 minutes.");
+        }
+        this.duration = duration;
+    }
+}
